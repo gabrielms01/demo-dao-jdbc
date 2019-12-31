8a3754d84f04ff4b1ddf1c880fbe4b156845a4b2
Gabriel Menezes <gabriel_oliveiramenezes@yahoo.com.br>
2019-12-31 17:10:43 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,10 +5,8 @@
  */
 package application;
 
-import java.util.Date;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 /**
@@ -23,11 +21,9 @@ public class Program {
     public static void main(String[] args) {
         // PROJETO- criando projetos e repositório Git- Aula 234
         
-        Department obj= new Department(1, "Books");
-        
-        Seller seller= new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-        
         SellerDao sellerDao= DaoFactory.createSellerDao();
+        
+        Seller seller= sellerDao.findById(3);
         
         System.out.println(seller);
     }
