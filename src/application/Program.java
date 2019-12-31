ff02b9d87f4f6b86ae29ef90c04b2f99b9d23d80
Gabriel Menezes <gabriel_oliveiramenezes@yahoo.com.br>
2019-12-31 16:29:00 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,9 @@
  */
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  *
@@ -20,7 +22,10 @@ public class Program {
         // PROJETO- criando projetos e repositório Git- Aula 234
         
         Department obj= new Department(1, "Books");
-        System.out.println(obj);
+        
+        Seller seller= new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        
+        System.out.println(seller);
     }
     
 }
